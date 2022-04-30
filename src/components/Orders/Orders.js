5c3e40791b114adf0ef0cfb6364e59a59b4cd67d
Mohammad Shahed <mohammadshahed5358@gmail.com>
2022-04-30 15:54:01 +0600

    cart updated

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css';
@@ -8,6 +10,13 @@ import './Orders.css';
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+
+    const handleRemoveProduct = product => {
+        const rest = cart.filter(pd => pd.id !== product.id);
+        setCart(rest);
+        removeFromDb(product.id)
+    }
+
     return (
         <div className="shop-container">
             <div className="review-items-container">
@@ -15,12 +24,17 @@ const Orders = () => {
                     cart.map(product =><ReviewItem 
                     key={product.id} 
                     product={product}
+                    handleRemoveProduct={handleRemoveProduct}
                     ></ReviewItem>)
                 }
 
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+                    <Link to='/inventory'>
+                        <button>Proceed Checkout</button>
+                    </Link>
+                </Cart>
             </div>
             
         </div>
