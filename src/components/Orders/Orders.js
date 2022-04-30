200cad811cb4d6eac25811f2b1d2b16e28a42bd0
Mohammad Shahed <mohammadshahed5358@gmail.com>
2022-04-30 14:59:04 +0600

    order cart added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
-        <div>
-            <h1>This is Orders : {products.length} </h1>
+        <div className="shop-container">
+            <div className="review-items-container">
+                {
+                    cart.map(product =><ReviewItem 
+                    key={product.id} 
+                    product={product}
+                    ></ReviewItem>)
+                }
+
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+            
         </div>
     );
 };
