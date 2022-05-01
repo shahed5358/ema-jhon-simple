529ac68c04b3e4731318598db8449a3a2c2d8a64
Mohammad Shahed <mohammadshahed5358@gmail.com>
2022-05-01 22:05:38 +0600

    sign up page create

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,26 +6,33 @@ const Login = () => {
   return (
     <div className="form-container">
       <div>
-        <h1 className="form-title">Please Login</h1>
+        <h1 className="form-title">Login</h1>
         <form>
         <div className="input-group">
-          <label htmlFor="email" name="email">
-            Email
-          </label>
-          <input type="email" name="email" id="" placeholder="Enter Email" />
-          <label htmlFor="password" name="password">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id=""
-            placeholder="Enter Password"
-          />
-        </div>
-        <input className="form-submit" type="submit" value="Login" />
+            <label htmlFor="email">
+              Email
+            </label>
+            <input type="email" name="email" id="" placeholder="Enter Email" required/>
+          </div>
+          <div className="input-group">
+          <label htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id=""
+              placeholder="Enter Password" required
+            />
+          </div>
+          <input className="form-submit" type="submit" value="Login" />
         </form>
-        <p>New to Ema-John ? <Link className="form-link" to="/signup">Create an account</Link></p>
+        <p>
+          New to Ema-John ?{" "}
+          <Link className="form-link" to="/signup">
+            Create an account
+          </Link>
+        </p>
       </div>
     </div>
   );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css';
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>Please SignUp</h1>
-        </div>
+        <div className="form-container">
+      <div>
+        <h1 className="form-title">Sign Up</h1>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">
+              Email
+            </label>
+            <input type="email" name="email" id="" placeholder="Enter Email" required/>
+          </div>
+          <div className="input-group">
+          <label htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id=""
+              placeholder="Enter Password" required
+            />
+          </div>
+          <div className="input-group">
+          <label htmlFor="confirm-password">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id=""
+              placeholder="Enter Password" required
+            />
+          </div>
+          <input className="form-submit" type="submit" value="Sign Up" />
+        </form>
+        <p>
+          Already have an account?{" "}
+          <Link className="form-link" to="/login">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
     );
 };
 
