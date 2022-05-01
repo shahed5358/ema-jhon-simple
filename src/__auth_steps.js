dbe12e5ebdfb8ad12b47c16fa37008999808a345
Mohammad Shahed <mohammadshahed5358@gmail.com>
2022-05-01 22:56:42 +0600

    create user handle

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,10 +1,14 @@
 /**
- * 1. create a new project in console.google.com
+ * 1. create a new firebase project in console.firebase.google.com
  * 2. npm install firebase
  * 3. create firebase.init.js and import getAuth to export app
  * 4. firebase settings> Authentication> enable email and password auth
  * 5. create login, signup and setup route
  * 6. attach form field handler and form submit handler
+ * 7. npm install --save react-firebase-hooks
+ * 8. useCreateUserWithEmailAndPassword form react-firebase-hooks
+ * 9. if user is create redirect to the expected page
+ * 10. useSignInWithEmailAndPassword for sign in
 */
 
 /**
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import './SignUp.css';
 
 const SignUp = () => {
@@ -8,6 +10,9 @@ const SignUp = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
 
     const  handleEmailBlur = event => {
       setEmail(event.target.value);
@@ -21,12 +26,22 @@ const SignUp = () => {
       setConfirmPassword(event.target.value);
     }
 
+    if(user){
+      navigate('/shop');
+    }
+
     const handleCreateUser = event => {
       event.preventDefault();
       if(password !== confirmPassword){
         setError('Your two passwords did not match');
         return;
       }
+      if(password.length < 6){
+        setError('Password must be at least 6 characters or longer')
+        return;
+      }
+
+      createUserWithEmailAndPassword(email, password);
     }
 
     return (
